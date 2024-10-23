604a25906da08f38a045655b54114932fb1fc722
BezborodovVV37 <blood371993@gmail.com>
2024-10-23 15:40:41 +0300

    Add files via upload

--- vb_module_10_4.py
+++ vb_module_10_4.py
@@ -1,77 +1,64 @@
-import threading
-import time
+from threading import Thread
 from queue import Queue
+from time import sleep
+from random import randint
 
 
 class Table:
-    def __init__(self, number):
+    def __init__(self, number, guest=None):
         self.number = number
-        self.is_busy = False
+        self.guest = guest
 
 
-class Cafe:
-    def __init__(self, tables):
-        self.tables = tables
-        self.queue = Queue()
-        self.customer_id = 1
+class Guest(Thread):
 
-    def customer_arrival(self):
-        for _ in range(20):
-            print(f"Посетитель номер {self.customer_id} прибыл.")
-            customer_thread = threading.Thread(target=self.serve_customer, args=(self.customer_id,))
-            customer_thread.start()
-            self.customer_id += 1
-            time.sleep(1)
-
-    def serve_customer(self, customer_id):
-        available_table = None
-        for table in self.tables:
-            if not table.is_busy:
-                available_table = table
-                break
-
-        if available_table:
-            available_table.is_busy = True
-            print(f"Посетитель номер {customer_id} сел за стол {available_table.number}.")
-            customer = Customer(customer_id, available_table)
-            customer.start()
-        else:
-            print(f"Посетитель номер {customer_id} ожидает свободный стол.")
-            self.queue.put(customer_id)
-
-    def release_table(self, customer_id, table):
-        print(f"Посетитель номер {customer_id} покушал и ушёл.")
-        table.is_busy = False
-        if not self.queue.empty():
-            next_customer_id = self.queue.get()
-            print(f"Посетитель номер {next_customer_id} сел за стол {table.number}.")
-            customer = Customer(next_customer_id, table)
-            customer.start()
-
-
-class Customer(threading.Thread):
-    def __init__(self, customer_id, table):
+    def __init__(self, name):
         super().__init__()
-        self.customer_id = customer_id
-        self.table = table
+        self.name = name
 
     def run(self):
-        time.sleep(5)
-        cafe.release_table(self.customer_id, self.table)
+        sleep(randint(3, 10))
 
 
-# Создаем столики в кафе
-table1 = Table(1)
-table2 = Table(2)
-table3 = Table(3)
-tables = [table1, table2, table3]
+class Cafe():
+    def __init__(self, *tables):
+        self.queue = Queue()
+        self.tables = list(tables)
 
-# Инициализируем кафе
-cafe = Cafe(tables)
+    def guest_arrival(self, *guests):
+        for guest in guests:
+            for table in self.tables:
+                if table.guest is None:
+                    table.guest = guest
+                    guest.start()
+                    print(f"{guest.name} сел(-а) за стол номер {table.number}")
+                    break
 
-# Запускаем поток для прибытия посетителей
-customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
-customer_arrival_thread.start()
+            else:
+                self.queue.put(guest)
+                print(f"{guest.name} в очереди")
 
-# Ожидаем завершения работы прибытия посетителей
-customer_arrival_thread.join()
+    def discuss_guests(self):
+        while not self.queue.empty() or any(table.guest is not None for table in self.tables):
+            for table in self.tables:
+                if table.guest and not table.guest.is_alive():
+                    print(f"{table.guest.name} покушал(-а) и ушёл(ушла)")
+                    print(f"Стол номер {table.number} свободен")
+                    table.guest = None
+                    if not self.queue.empty():
+                        next_guest = self.queue.get()
+                        table.guest = next_guest
+                        next_guest.start()
+                        print(f"{next_guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}")
+
+
+tables = [Table(number) for number in range(1, 6)]  # Создание столов
+
+guests_names = ['Коля', 'Ира', 'Даша', 'Света', 'Вася', 'Саша',  # Имена гостей
+                'Катя', 'Вика', 'Лена', 'Дима', 'Рома', 'Андрей',
+                'Кирил', 'Маша', 'Клава', 'Стас', 'Слава', 'Женя'
+                ]
+guests = [Guest(name) for name in guests_names]  # Создание гостей
+cafe = Cafe(*tables)  # Заполнение кафе столами
+cafe.guest_arrival(*guests)  # Приём гостей
+cafe.discuss_guests()  # Обслуживание гостей
